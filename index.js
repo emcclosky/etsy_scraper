4ebc367bc4d4817b2bb5401fc09c81bd24f3af0c
Elizabeth Arinder <emcclosky@gmail.com>
2018-05-20 12:17:48 -0400

    Dependencies injected and boilerplate added.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+
+app.get('/scrape', function(req, res){
+
+
+
+
+})
+
+
+app.listen('8081');
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
